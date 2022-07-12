edd16e28b3c58f248f0e8189be8a4743f776f231
vigli-Lee <wnsqjadl@gmail.com>
2022-07-12 21:06:24 +0900

    argument array to Iterator

--- app/src/main/java/com/vigli/deepdive/example/cleancodechapter14/Args.java
+++ app/src/main/java/com/vigli/deepdive/example/cleancodechapter14/Args.java
@@ -5,27 +5,27 @@ import java.util.*;
 
 public class Args {
     private String schema;
-    private String[] args;
     private boolean valid = true;
     private Set<Character> unexpectedArguments = new TreeSet<Character>();
     private Map<Character, ArgumentMarshaler> marshalers = new HashMap();
     private Set<Character> argsFound = new HashSet<Character>();
-    private int currentArgument;
+    private Iterator<String> currentArgument;
     private char errorArgumentId = '\0';
     private String errorParameter = "TILT";
     private ErrorCode errorCode = ErrorCode.OK;
+    private List<String> argsList;
 
     private enum ErrorCode {
         OK, MISSING_STRING, MISSING_INTEGER, INVALID_INTEGER, UNEXPECTED_ARGUMENT}
 
     public Args(String schema, String[] args) throws ParseException {
         this.schema = schema;
-        this.args = args;
+        argsList = Arrays.asList(args);
         valid = parse();
     }
 
     private boolean parse() throws ParseException {
-        if (schema.length() == 0 && args.length == 0)
+        if (schema.length() == 0 && argsList.size() == 0)
             return true;
         parseSchema();
         try {
@@ -79,8 +79,8 @@ public class Args {
     }
 
     private boolean parseArguments() throws ArgsException {
-        for (currentArgument = 0; currentArgument < args.length; currentArgument++) {
-            String arg = args[currentArgument];
+        for (currentArgument = argsList.iterator(); currentArgument.hasNext();) {
+            String arg = currentArgument.next();
             parseArgument(arg);
         }
         return true;
@@ -106,6 +106,7 @@ public class Args {
 
     private boolean setArgument(char argChar) throws ArgsException {
         ArgumentMarshaler m = marshalers.get(argChar);
+        if (m == null) return false;
         try {
             if (m instanceof BooleanArgumentMarshaler)
                 setBooleanArg(m);
@@ -113,8 +114,6 @@ public class Args {
                 setStringArg(m);
             else if (m instanceof IntegerArgumentMarshaler)
                 setIntArg(m);
-            else
-                return false;
         } catch (ArgsException e) {
             valid = false;
             errorArgumentId = argChar;
@@ -124,12 +123,11 @@ public class Args {
     }
 
     private void setIntArg(ArgumentMarshaler m) throws ArgsException {
-        currentArgument++;
         String parameter = null;
         try {
-            parameter = args[currentArgument];
+            parameter = currentArgument.next();
             m.set(parameter);
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (NoSuchElementException e) {
             errorCode = ErrorCode.MISSING_INTEGER;
             throw new ArgsException();
         } catch (ArgsException e) {
@@ -140,21 +138,16 @@ public class Args {
     }
 
     private void setStringArg(ArgumentMarshaler m) throws ArgsException {
-        currentArgument++;
         try {
-            m.set(args[currentArgument]);
-        } catch (ArrayIndexOutOfBoundsException e) {
+            m.set(currentArgument.next());
+        } catch (NoSuchElementException e) {
             errorCode = ErrorCode.MISSING_STRING;
             throw new ArgsException();
         }
     }
 
-    private void setBooleanArg(ArgumentMarshaler m) {
-        try {
-            m.set("true");
-        } catch (ArgsException e) {
-
-        }
+    private void setBooleanArg(ArgumentMarshaler m) throws ArgsException {
+        m.set("true");
     }
 
     public int cardinality() {
